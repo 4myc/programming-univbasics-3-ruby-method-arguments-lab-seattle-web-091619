ba1d8a3ad221d70e945f4ffff91ac2f2d4cf2287
Amy C <missch3n@gmail.com>
2019-09-11 08:30:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,9 +6,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def instruction_with_language_optional(name, language)
 
-end
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
